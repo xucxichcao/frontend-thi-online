530e87b9357f0dd8651e8cdad85b998801ef2991
Cao Trọng Nghĩa <thekrualfox@gmail.com>
2021-09-23 17:58:19 +0700

    finish sign ip

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,39 +1,51 @@
 <template>
   <Row id="signin" type="flex" space="20" justify="center" space-x="12">
-    <Cell :xs="22" :sm="20" :md="16">
+    <Cell :xs="22" :sm="20" :md="18" width="12">
       <div class="h-panel">
         <div class="h-panel-bar grey-bg">
           <span class="h-panel-title">Trang đăng nhập thi online</span>
         </div>
         <div class="h-panel-body">
-          <Row space="10" type="flex" justify="center" align="center">
-            <Cell :xs="22" :sm="20" :md="11">
+          <Row :space="20" type="flex" justify="center" align="center">
+            <Cell :xs="22" :sm="20" :md="12" width="10">
               <Form
                 mode="block"
                 ref="form"
                 :validOnChange="true"
                 :showErrorTip="true"
               >
-                <FormItem>
+                <FormItem :showLabel="false">
                   <input
                     placeholder="Tên đăng nhập"
                     type="text"
                     v-model="user.email"
                   />
                 </FormItem>
-                <FormItem>
+                <FormItem :showLabel="false">
                   <input
                     placeholder="Mật khẩu"
                     type="password"
                     v-model="user.password"
                   />
                 </FormItem>
-                <FormItem>
-                  <Button color="primary">Đăng nhập</Button>
+                <FormItem :showLabel="false">
+                  <Button block color="primary">Đăng nhập</Button>
                 </FormItem>
               </Form>
             </Cell>
-            <Cell :xs="22" :sm="20" :md="11"></Cell>
+            <Cell :xs="22" :sm="20" :md="12" width="10">
+              <Row class="infoRow" :space-y="20">
+                <Cell width="24">
+                  <router-link to="/"><span>Quên mật khẩu?</span></router-link>
+                </Cell>
+                <Cell width="24">
+                  <span
+                    >Trình duyệt của bạn cần phải mở chức năng quản lí
+                    cookie</span
+                  >
+                </Cell>
+              </Row>
+            </Cell>
           </Row>
         </div>
       </div>
@@ -54,7 +66,13 @@ export default {
 .grey-bg {
   background-color: rgba(0, 0, 0, 0.03);
 }
-.h-form-item {
+.h-form {
   padding-bottom: 0;
+}
+.infoRow {
+  text-align: start;
+}
+.h-panel-body {
+  padding-top: 2rem;
 }
 </style>
