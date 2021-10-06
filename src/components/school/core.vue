82f7c0f7c6c94084647eb6313b0d4a3303852ba2
Mạnh Hùng Huỳnh <huynhmanhhung2000@gmail.com>
2021-10-06 20:21:44 +0700

    hung

---
+++ src/components/school/core.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="school-core">
+    <div class="h-panel">
+      <div class="h-panel-body">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
